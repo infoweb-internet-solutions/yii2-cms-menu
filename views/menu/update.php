26b72b60135f4b8dbbed50a602508b6a022b3f2c
Fabio Maggio <fabio@infoweb.be>
2014-10-28 15:14:42 +0100

    Fix page title

--- views/menu/update.php
+++ views/menu/update.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => Yii::t('infoweb/menu', 'Menu'),
-]) . ' ' . $model->name;
+]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('infoweb/menu', 'Menus'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['update', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
