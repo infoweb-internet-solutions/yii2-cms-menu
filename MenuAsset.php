8665fa1d6c1935063b7abbb5b1571b1d49fc3778
Fabio Maggio <fabio@infoweb.be>
2014-11-06 11:51:04 +0100

    Fix #19

--- MenuAsset.php
+++ MenuAsset.php
@@ -8,12 +8,11 @@ class MenuAsset extends AssetBundle
 {
     public $sourcePath = '@infoweb/menu/assets/';
     public $css = [
-        '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/smoothness/jquery-ui.css',
+        '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css',
         'css/main.css',
     ];
     public $js = [
-        '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js',
-        '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.js',
+        '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js',
         'js/jquery.mjs.nestedSortable.js',
         'js/nested-sortable-settings.js',
         'js/floatThead/jquery.floatThead.min.js',
